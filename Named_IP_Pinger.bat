959882594163f13537803a9266e8fed20492a7a1
X9Apollo <63824898+X9Apollo@users.noreply.github.com>
2021-03-28 00:47:27 +0000

    Add files via upload

---
+++ Named_IP_Pinger.bat
@@ -0,0 +1,121 @@
+@echo off
+:a
+mode 100,50
+color C
+
+title Custom IP Pinger With Your Name : Made by @X9 APOLLO#6358
+
+:greeting
+cls
+
+echo  .d8b.  d8888b.  .d88b.  db      db       .d88b.     .d88b.  d8888b. 
+echo d8' `8b 88  `8D .8P  Y8. 88      88      .8P  Y8.   .8P  Y8. 88  `8D 
+echo 88ooo88 88oodD' 88    88 88      88      88    88   88    88 88oodD' 
+echo 88~~~88 88~~~   88    88 88      88      88    88   88    88 88~~~   
+echo 88   88 88      `8b  d8' 88booo. 88booo. `8b  d8'   `8b  d8' 88      
+echo YP   YP 88       `Y88P'  Y88888P Y88888P  `Y88P'     `Y88P'  88      
+                                                                     
+echo           #-----------------# #-----------#
+echo           # @X9 APOLLO#6358 # # IP PINGER #
+echo           #-----------------# #-----------#
+echo !Name doesnt get logged just for a better experience!
+set /p Name= Enter A Name:
+title Oi %Name% Target=
+set /p IP= Smack Ip here %Name%:
+title Oi %Name% Target= %IP%
+echo Target = %IP%
+echo RIP WIFI ----%IP% -- APOLLO OP
+title WiFi eat shit nerd
+:top
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been taken down...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+echo -- %Name% OP --
+title A
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+echo -- %Name% OP --
+title AP
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+echo -- %Name% OP --
+title APO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+echo -- %Name% OP --
+title APOL
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+echo -- %Name% OP --
+title APOLL
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+echo -- %Name% OP --
+title APOLLO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+echo -- %Name% OP --
+title APOLLO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+echo -- %Name% OP --
+title APOLLO T
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+echo -- %Name% OP --
+title APOLLO TO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+echo -- %Name% OP --
+title APOLLO TOO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+echo -- %Name% OP --
+title APOLLO TOO
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+echo -- %Name% OP --
+title APOLLO TOO O
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+echo -- %Name% OP --
+title APOLLO TOO OP
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+echo -- %Name% OP --
+title APOLLO TOO OP LOL
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+echo -- %Name% OP --
+title Get fucked
+echo Made by @X9 APOLLO#6358
+echo %Name% going for %IP% whos gonna get fucked
+GoTo top
